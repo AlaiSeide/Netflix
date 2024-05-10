96ee37c861b5fc6ae4afc40f8872e9b4fc4aaa5a
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2024-05-10 21:22:58 +0200

    Criei url dinamico

--- filme/urls.py
+++ filme/urls.py
@@ -5,12 +5,13 @@
 
 # AQUI DEFINIMOS AS URLS DO NOSSO SITE
 from django.urls import path
-from .views import homepage, homefilmes
+from .views import Homepage, Homefilmes, Detalhesfilme
 
 # Define os padrões de URL para o seu aplicativo Django
 urlpatterns = [
-    # homepage é a minha view
-    path('', homepage),
-    path('filmes/', homefilmes),
-
+    # Homepage é a minha view
+    path('', Homepage.as_view()),
+    path('filmes/', Homefilmes.as_view()),
+    # o pk -> id que voce passou no seu url, é o primary_key do modelo da classe Detalhesfilme, o modelo da classe que é o Filme
+    path('filmes/<int:pk>', Detalhesfilme.as_view()),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
 from .models import Filme
+from django.views.generic import TemplateView, ListView, DetailView
+
 
 # Create your views here.
+# def homepage(request):
+#     # request pode ser GET, POST
+#     return render(request, 'homepage.html')
 
-def homepage(request):
-    # request pode ser GET, POST
-    return render(request, 'homepage.html')
+# def homefilmes(request):
+#     context = {}
+#     # pegando todos os objetos do meu banco de dados
+#     # uma lista com todos os filmes do meu banco de dados
+#     lista_filmes = Filme.objects.all()
+#     print(lista_filmes)
+#     # criando uma chave no dicionario
+#     context['lista_filmes'] = lista_filmes
+#     return render(request, 'homefilmes.html', context)
+
+# TemplateView A classe TemplateView no Django é usada para exibir páginas da web que não exigem nenhum processamento de dados do banco de dados, como páginas estáticas, páginas de ajuda ou páginas de contato. Em outras palavras, é usada quando você só precisa renderizar um modelo de template sem lidar diretamente com dados do banco de dados.
+class Homepage(TemplateView):
+    template_name = 'homepage.html'
 
 
-def homefilmes(request):
-    context = {}
-    # pegando todos os objetos do meu banco de dados
-    # uma lista com todos os filmes do meu banco de dados
-    lista_filmes = Filme.objects.all()
-    print(lista_filmes)
-    # criando uma chave no dicionario
-    context['lista_filmes'] = lista_filmes
-    return render(request, 'homefilmes.html', context)
 
+
+# ListView A classe ListView no Django é usada para mostrar uma lista de itens de um modelo específico em uma página da web. Ela ajuda a buscar os itens do banco de dados e exibi-los em um formato fácil de visualizar para os usuários.
+class Homefilmes(ListView):
+    template_name = 'homefilmes.html' # O nome do arquivo de template que será usado para mostrar a lista de Filmes
+    model = Filme
+    context_object_name = 'lista_filmes'  # O nome da variável que será usada no template para acessar os itens o object_list é o padrao caso nao definimos o context
+
+
+class Detalhesfilme(DetailView):
+    # object é o padrao caso nao definimos o context
+    template_name = 'detalhesfilme.html' # O nome do arquivo de template que será usado para mostrar os detalhes do filme
+    model = Filme
