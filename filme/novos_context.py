09af68291bc5ca03f05249dc04f4439ced0ba751
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2024-05-12 15:26:32 +0200

    personalizar homefilmes

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -4,9 +4,9 @@ from .models import Filme
 
 def lista_filmes_recentes(request):
     # Definindo a função lista_filmes_recentes que recebe um objeto request como parâmetro
-    # Buscando todos os filmes no banco de dados, ordenando-os em ordem decrescente de data de criação e pegando os 10 primeiros
+    # Buscando todos os filmes no banco de dados, ordenando-os em ordem decrescente de data de criação e pegando os 8 primeiros
     # 'data_criacao' é o nome do campo da sua tabela de filmes que representa a data de criação
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:8]
 
     # Retornando um dicionário com a chave 'lista_filmes_recentes' e a lista de filmes como valor
     return {'lista_filmes_recentes': lista_filmes}
@@ -14,8 +14,8 @@ def lista_filmes_recentes(request):
 
 
 def lista_filmes_emalta(request): # Definindo a função lista_filmes_emalta que recebe um objeto request como parâmetro
-    # Buscando todos os filmes no banco de dados, ordenando-os em ordem decrescente de visualizações e pegando os 10 primeiros
-    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:10] 
+    # Buscando todos os filmes no banco de dados, ordenando-os em ordem decrescente de visualizações e pegando os 8 primeiros
+    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:8] 
 
      # Retornando um dicionário com a chave 'lista_filmes_emalta' e a lista de filmes como valor
     return {'lista_filmes_emalta': lista_filmes} 
